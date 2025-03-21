be1e51e3dee355cb6438f42ea1e483da1fdb497e
Michał Górny <mgorny@gentoo.org>
2025-03-21 19:56:12 +0100

    Bump to v17
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- gpep517/__init__.py
+++ gpep517/__init__.py
@@ -1,6 +1,6 @@
-# (c) 2022-2024 Michał Górny
+# (c) 2022-2025 Michał Górny
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 """Python package builder and installer for non-pip-centric world"""
 
-__version__ = "16"
+__version__ = "17"
